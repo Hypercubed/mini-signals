32f2aef9445b4fcd101e0de7c2fb463c526cd37b
Easy <isak.wertwein@gmail.com>
2016-12-26 16:02:20 +0100

    Typescript definition fixes

--- typings/mini-signals.d.ts
+++ typings/mini-signals.d.ts
@@ -3,12 +3,13 @@ export interface MiniSignalBinding {
 }
 
 export interface MiniSignal {
+	MiniSignalBinding: { new (): MiniSignalBinding };
 	new (): MiniSignal;
-	handlers (exists: boolean = false): MiniSignalBinding[];
+	handlers (exists?: boolean): MiniSignalBinding[];
 	has (node: MiniSignalBinding): boolean;
 	dispatch (...args: any[]): boolean;
-	add (fn: Function, thisArg: any = null): MiniSignalBinding;
-	once (fn: Function, thisArg: any = null): MiniSignalBinding;
+	add (fn: Function, thisArg?: any): MiniSignalBinding;
+	once (fn: Function, thisArg?: any): MiniSignalBinding;
 	detach (node: MiniSignalBinding): this | null;
 	detachAll (): this | null;
 }
