98a682dc49e17273c88ba3814db804572a5408a2
Easy <iswer1@gmail.com>
2017-05-06 00:05:11 +0200

    Put everything inside module

--- typings/mini-signals.d.ts
+++ typings/mini-signals.d.ts
@@ -1,20 +1,20 @@
-declare namespace MiniSignal {
-	interface MiniSignalBinding {
-		detach(): boolean;
-	}
-}
-
-declare class MiniSignal {
-	constructor();
-	handlers(exists?: boolean): MiniSignal.MiniSignalBinding[] | boolean;
-	has(node: MiniSignal.MiniSignalBinding): boolean;
-	dispatch(...args: any[]): boolean;
-	add(fn: Function, thisArg?: any): MiniSignal.MiniSignalBinding;
-	once(fn: Function, thisArg?: any): MiniSignal.MiniSignalBinding;
-	detach(node: MiniSignal.MiniSignalBinding): MiniSignal;
-	detachAll(): MiniSignal;
-}
-
 declare module "mini-signals" {
+	namespace MiniSignal {
+		interface MiniSignalBinding {
+			detach(): boolean;
+		}
+	}
+
+	class MiniSignal {
+		constructor();
+		handlers(exists?: boolean): MiniSignal.MiniSignalBinding[] | boolean;
+		has(node: MiniSignal.MiniSignalBinding): boolean;
+		dispatch(...args: any[]): boolean;
+		add(fn: Function, thisArg?: any): MiniSignal.MiniSignalBinding;
+		once(fn: Function, thisArg?: any): MiniSignal.MiniSignalBinding;
+		detach(node: MiniSignal.MiniSignalBinding): MiniSignal;
+		detachAll(): MiniSignal;
+	}
+
 	export = MiniSignal;
 }
